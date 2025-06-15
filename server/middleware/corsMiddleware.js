f05008a3aaed634a926162304aa5a0407030c81d
Antonio Gómez <antgomdgz@gmail.com>
2025-06-15 16:33:49 +0200

    corsmiddleware3

--- server/middleware/corsMiddleware.js
+++ server/middleware/corsMiddleware.js
@@ -1,26 +1,29 @@
 const allowedOrigins = [
-  'https://todo-controlado-pro.vercel.app',
-  'https://todo-controlado-pro-git-main-antonios-projects-99da8543.vercel.app',
-  'https://todo-controlado-nio9tpp2j-antonios-projects-99da8543.vercel.app',
-  'http://localhost:5173'
+  "https://todo-controlado-pro.vercel.app",
+  "https://todo-controlado-pro-git-main-antonios-projects-99da8543.vercel.app",
+  "https://todo-controlado-nio9tpp2j-antonios-projects-99da8543.vercel.app",
+  "http://localhost:5173",
 ];
 
 function corsMiddleware(req, res, next) {
   const origin = req.headers.origin;
 
-  if (!origin || allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin || '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (origin && allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
       return res.sendStatus(200);
     }
 
     next();
   } else {
-    res.status(403).send('Not allowed by CORS');
+    res.status(403).send("Not allowed by CORS");
   }
 }
 

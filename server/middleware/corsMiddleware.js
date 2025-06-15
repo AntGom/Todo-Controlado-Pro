7c7c14c006b8743d56028d30144b8b2294d72a36
Antonio Gómez <antgomdgz@gmail.com>
2025-06-15 15:52:28 +0200

    corsMiddleware

--- server/middleware/corsMiddleware.js
+++ server/middleware/corsMiddleware.js
@@ -1,7 +1,19 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://todo-controlado-pro.vercel.app/'
+];
+
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' ? false : 'http://localhost:5173', 
+  origin: function (origin, callback) {
+    // Permitir peticiones sin origin (como desde curl o Postman) y las de origen permitido
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
